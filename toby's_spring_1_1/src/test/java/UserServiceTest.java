99d57a5f20703e17fe8436ddafa320f98fa8fbc9
foameraserblue@gmail.com <foameraserblue@gmail.com>
2022-04-03 22:27:32 +0900

    6장 426 ~ 429 Mockito 프레임워크를 이용한 단위 테스트

--- toby's_spring_1_1/src/test/java/UserServiceTest.java
+++ toby's_spring_1_1/src/test/java/UserServiceTest.java
@@ -10,9 +10,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.PlatformTransactionManager;
 import testFactory.TestFactory;
@@ -26,6 +28,7 @@ import static com.foameraserblue.service.UserServiceImpl.MIN_LOGCOUNT_FOR_SILVER
 import static com.foameraserblue.service.UserServiceImpl.MIN_RECCOMEND_FOR_GOLD;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.fail;
+import static org.mockito.Mockito.*;
 
 public class UserServiceTest {
 
@@ -84,9 +87,10 @@ public class UserServiceTest {
     @DirtiesContext // 컨텍스트의 di 를 변경하는 테스트라는 의미
     public void upgradeLevels() throws SQLException {
         // 가짜 DAO 객체
-        MockUserDao mockUserDao = new MockUserDao(this.users);
+        UserDao mockUserDao = mock(UserDao.class);
+        when(mockUserDao.getAll()).thenReturn(this.users);
         // 가짜 MailSender 객체
-        MockMailSender mockMailSender = new MockMailSender();
+        MailSender mockMailSender = mock(MailSender.class);
         // 고립된 테스트에서는 테스트 오브젝트를 직접 생성해준다
         // 고립된 테스트에서는 독립적으로 동작하는 대상을 사용하기 때문에 Bean 을 통해 DI 받을 필요가 없다
         UserServiceImpl userServiceImpl = new UserServiceImpl(mockUserDao, mockMailSender);
@@ -94,18 +98,18 @@ public class UserServiceTest {
         // 비지니스 로직 실행
         userServiceImpl.upgradeLevels();
 
-        // 실행된 로직을 검증할 mock 객체에서 출력내역 확인
-        List<User> updated = mockUserDao.getUpdated();
+        verify(mockUserDao, times(2)).update(any(User.class));
+        verify(mockUserDao).update(users.get(1));
+        Assert.assertEquals(users.get(1).getLevel(), Level.SILVER);
+        verify(mockUserDao).update(users.get(3));
+        Assert.assertEquals(users.get(3).getLevel(), Level.GOLD);
 
-        Assert.assertEquals(updated.size(), 2);
-        checkUserAndLevel(updated.get(0), "2번아디", Level.SILVER);
-        checkUserAndLevel(updated.get(1), "4번아디", Level.GOLD);
-
-        // 메일 발송 내역 확인
-        List<String> request = mockMailSender.getRequest();
-        assertEquals(request.size(), 2);
-        assertEquals(request.get(0), users.get(1).getEmail());
-        assertEquals(request.get(1), users.get(3).getEmail());
+        ArgumentCaptor<SimpleMailMessage> mailMessageArg =
+                ArgumentCaptor.forClass(SimpleMailMessage.class);
+        verify(mockMailSender, times(2)).send(mailMessageArg.capture());
+        List<SimpleMailMessage> mailMessages = mailMessageArg.getAllValues();
+        Assert.assertEquals(mailMessages.get(0).getTo()[0], users.get(1).getEmail());
+        Assert.assertEquals(mailMessages.get(1).getTo()[0], users.get(3).getEmail());
 
 
     }
